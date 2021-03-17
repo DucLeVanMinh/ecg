21ae388b7f3e0cdfdc5cbad74aedd8a4c6193e32
DucLe <le.m.duc86@gmail.com>
2021-03-17 22:51:35 +0000

    Change on some files

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ fi
 
 
 pip -q install -r requirements.txt
-# pip install --upgrade $TF==1.9.0
+# pip install --upgrade $TF==1.8.0
 
 pip -q install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.8.0-py3-none-any.whl
 
